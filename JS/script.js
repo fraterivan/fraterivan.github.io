536a4fc8531da57e20b0185d25a80dac2d5c6999
Fráter Iván <fraterivan03@gmail.com>
2020-07-28 23:56:01 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -35,6 +35,50 @@ function openTab(event, tab) {
   article.removeAttribute("hidden");
 };
 
+dragElement(document.getElementById("dragDiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+  
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+  
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+  
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 function notWorking(){
   window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+function help(){
+  location.href = "https://fraterivan.tk/about.html";
+}
+
+function close(){
+  location.href = "https://fraterivan.tk/index.html";
 }
